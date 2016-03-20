e92538fa9d94efa767b968dc416ade837272a837
lennart.vandamme@gmail.com <Lennart@DESKTOP-47E9N9F.lan>
2016-03-20 13:55:48 +0100

    Signed-off-by: lennart.vandamme@gmail.com <Lennart@DESKTOP-47E9N9F.lan>

--- Winkel/src/Winkel.java
+++ Winkel/src/Winkel.java
@@ -1,30 +1,72 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.spec.ECFieldFp;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
+import java.security.spec.EllipticCurve;
+
+import javax.crypto.KeyAgreement;
 
 public class Winkel {
-	
+
 	private String winkelNaam;
-	
-	public Winkel(String winkelNaam){
+
+	public Winkel(String winkelNaam) {
 		this.winkelNaam = winkelNaam;
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 	}
-	
+
 	public String getWinkelNaam() {
 		return winkelNaam;
 	}
-	
-	public void puntenToevoegen(int punten){
+
+	public void puntenToevoegen(int punten) {
 		System.out.println("Er worden " + punten + " punten toegevoegd");
 	}
-	
-	public void startGUI(){
+
+	public void startGUI() {
 		WinkelGUI gui = new WinkelGUI(this);
 		gui.setVisible(true);
+	}
+
+	private void setUpKeyAgreement() throws NoSuchAlgorithmException, NoSuchProviderException,
+			InvalidAlgorithmParameterException, InvalidKeyException {
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDH", "BC");
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+		EllipticCurve curve = new EllipticCurve(
+				new ECFieldFp(new BigInteger("fffffffffffffffffffffffffffffffeffffffffffffffff", 16)),
+				new BigInteger("fffffffffffffffffffffffffffffffeffffffffffffffff", 16),
+				new BigInteger("fffffffffffffffffffffffffffffffeffffffffffffffff", 16));
+
+		ECParameterSpec ecSpec = new ECParameterSpec(curve,
+				new ECPoint(new BigInteger("fffffffffffffffffffffffffffffffefffffffffffffffc", 16),
+						new BigInteger("fffffffffffffffffffffffffffffffefffffffffffffffc", 16)),
+				new BigInteger("fffffffffffffffffffffffffffffffefffffffffffffffc", 16), 1);
+
+		keyGen.initialize(ecSpec, random);
+		KeyAgreement aKeyAgree = KeyAgreement.getInstance("ECDH", "BC");
+		KeyPair aPair = keyGen.generateKeyPair();
+		KeyAgreement bKeyAgree = KeyAgreement.getInstance("ECDH", "BC");
+		KeyPair bPair = keyGen.generateKeyPair();
+
+		aKeyAgree.init(aPair.getPrivate());
+		bKeyAgree.init(bPair.getPrivate());
+
+		aKeyAgree.doPhase(bPair.getPublic(), true);
+		bKeyAgree.doPhase(aPair.getPublic(), true);
+
+		MessageDigest hash = MessageDigest.getInstance("SHA1", "BC");
+
+		System.out.println(new String(hash.digest(aKeyAgree.generateSecret())));
+		System.out.println(new String(hash.digest(bKeyAgree.generateSecret())));
 	}
 
 }
